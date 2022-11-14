63c71bec577b73b79295eafc46f61d32a5c55e73
Rasil07 <rasilgrt@gmail.com>
2022-11-14 20:30:47 +0545

    removed post schema

---
+++ prisma/migrations/20221114032516_created_user_updated_post/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `name` on the `Post` table. All the data in the column will be lost.
+  - Added the required column `description` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `title` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userId` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" DROP CONSTRAINT "Post_pkey",
+DROP COLUMN "name",
+ADD COLUMN     "description" TEXT NOT NULL,
+ADD COLUMN     "title" TEXT NOT NULL,
+ADD COLUMN     "userId" TEXT NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Post_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Post_id_seq";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" VARCHAR(255) NOT NULL,
+    "middleName" VARCHAR(255) NOT NULL,
+    "lastName" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
